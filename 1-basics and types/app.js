211941441c57caad817b9363c83d3156ebba3974
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-01-08 19:36:27 +0100

    JS native type check safety

--- 1-basics and types/app.js
+++ 1-basics and types/app.js
@@ -1,7 +1,10 @@
 function add(n1, n2) {
+    if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+        throw new Error('Incorrect input');
+    }
     return n1 + n2;
 }
-var number1 = 3;
+var number1 = '3';
 var number2 = 7.1;
 var result = add(number1, number2);
 console.log('Result', result);
--- 1-basics and types/app.ts
+++ 1-basics and types/app.ts
@@ -1,8 +1,12 @@
 function add(n1: number, n2: number) {
-  return n1 + n2;
+
+    if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+        throw new Error('Incorrect input')
+    }
+    return n1 + n2;
 }
 
-const number1 = 3;
+const number1 = '3';
 const number2 = 7.1;
 
 const result = add(number1, number2);
